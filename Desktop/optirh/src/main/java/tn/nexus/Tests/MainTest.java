a0bc4568e6deda7bba5edf381cc29f189eb3a84d
Ahmed Trabelsi <92649043+AhmedTrabelsy@users.noreply.github.com>
2025-02-08 11:13:07 +0100

    connect to db

---
+++ Desktop/optirh/src/main/java/tn/nexus/Tests/MainTest.java
@@ -0,0 +1,10 @@
+package tn.nexus.Tests;
+
+import tn.nexus.Utils.DBConnection;
+
+public class MainTest {
+    public static void main(String[] args) {
+        DBConnection db = DBConnection.getInstance();
+    }
+    
+}
