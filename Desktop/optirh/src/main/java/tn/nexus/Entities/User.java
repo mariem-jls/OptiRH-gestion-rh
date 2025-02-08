4db5933e7ee4d117638a8b7d891e0a62b951886a
Ahmed Trabelsi <92649043+AhmedTrabelsy@users.noreply.github.com>
2025-02-08 11:13:17 +0100

    create user entity

---
+++ Desktop/optirh/src/main/java/tn/nexus/Entities/User.java
@@ -0,0 +1,83 @@
+package tn.nexus.Entities;
+
+public class User {
+
+    private int id;
+    private String nom;
+    private String email;
+    private String motDePasse;
+    private String role;
+    private String address;
+
+    public User() {
+    }
+
+    public User(int id, String nom, String email, String motDePasse, String role, String address) {
+        this.id = id;
+        this.nom = nom;
+        this.email = email;
+        this.motDePasse = motDePasse;
+        this.role = role;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", email='" + email + '\'' +
+                ", motDePasse='" + motDePasse + '\'' +
+                ", role='" + role + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
