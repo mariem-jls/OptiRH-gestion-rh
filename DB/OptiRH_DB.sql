9b6363f9b96e7ac4557c98ef130fa316813dd2c0
Oumaima <bouhanioumaima2@gmail.com>
2025-02-26 20:53:11 +0100

    Modification tables Base de donnée et front Gestion Mission

--- DB/OptiRH_DB.sql
+++ DB/OptiRH_DB.sql
@@ -112,3 +112,29 @@ CREATE TABLE Conges (
     utilisateur_id INT,
     FOREIGN KEY (utilisateur_id) REFERENCES Utilisateur(id) ON DELETE CASCADE
 );
+CREATE TABLE `missions` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `titre` VARCHAR(100) NOT NULL,
+  `description` TEXT DEFAULT NULL,
+  `status` ENUM('To Do','In Progress','Done') DEFAULT 'To Do',
+  `project_id` INT(11) DEFAULT NULL,
+  `assigned_to` INT(11) DEFAULT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_terminer` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `project_id` (`project_id`),
+  KEY `assigned_to` (`assigned_to`),
+  CONSTRAINT `missions_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`),
+  CONSTRAINT `missions_ibfk_2` FOREIGN KEY (`assigned_to`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `projects` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(100) NOT NULL,
+  `description` TEXT DEFAULT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` INT(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `created_by` (`created_by`),
+  CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
