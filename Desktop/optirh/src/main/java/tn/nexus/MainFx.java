29140b3f1d05f92a383074b229381922bfefe8eb
Oumaima <bouhanioumaima2@gmail.com>
2025-03-04 19:49:04 +0100

    Ajout Img

--- Desktop/optirh/src/main/java/tn/nexus/MainFx.java
+++ Desktop/optirh/src/main/java/tn/nexus/MainFx.java
@@ -35,7 +35,7 @@ public class MainFx extends Application {
 
         // FXMLLoader loader = new
         // FXMLLoader(getClass().getResource("/formations/AjouterFormation.fxml"));
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/reclamation/listereclamationfront.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Users/ListUsers.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
 
