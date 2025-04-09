de13f59ba70ca8c91be0ac62aa7c43a30fb7e7bd
mariem-jls <197058634+mariem-jls@users.noreply.github.com>
2025-04-09 16:33:14 +0100

    Dashboard updes

--- Web/optiRH/src/Controller/BaseController.php
+++ Web/optiRH/src/Controller/BaseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\OffreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,8 +17,15 @@ class BaseController extends AbstractController
     }
 
     #[Route('/', name: 'base')]
-    public function index(): Response
+    public function index(OffreRepository $offreRepository): Response
     {
-        return $this->render('FrontOffice/pages-coming-soon.html.twig');
+        // Récupérer les offres actives
+        $offres = $offreRepository->findActiveOffres();
+
+        return $this->render('front-home/index.html.twig', [
+            'controller_name' => 'FrontController',
+            'offres' => $offres, // Passer les offres ici
+
+        ]);
     }
 }
