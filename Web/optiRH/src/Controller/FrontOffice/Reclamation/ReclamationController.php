40b4cb2bb911caa897f0c2bd95a0951708264162
Ahmed Trabelsi <92649043+AhmedTrabelsy@users.noreply.github.com>
2025-04-16 19:39:43 +0100

    fix the is granted import string

--- Web/optiRH/src/Controller/FrontOffice/Reclamation/ReclamationController.php
+++ Web/optiRH/src/Controller/FrontOffice/Reclamation/ReclamationController.php
@@ -3,15 +3,15 @@
 
 namespace App\Controller\FrontOffice\Reclamation;
 
-use App\Entity\Reclamation;
 use App\Entity\Reponse;
+use App\Entity\Reclamation;
 use App\Form\ReclamationType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/frontReclamation')]
 class ReclamationController extends AbstractController
