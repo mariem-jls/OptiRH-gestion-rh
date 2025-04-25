99ed3fd90f2e47fcc91952717db1319f9e1b5fdf
Ahmed Trabelsi <92649043+AhmedTrabelsy@users.noreply.github.com>
2025-04-25 05:06:19 +0100

    run the latest migration

---
+++ Web/optiRH/migrations/Version20250425040500.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250425040500 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE notifications (id INT AUTO_INCREMENT NOT NULL, recipient_id INT DEFAULT NULL, message VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', is_read TINYINT(1) NOT NULL, type VARCHAR(50) NOT NULL, context JSON DEFAULT NULL, route_name VARCHAR(255) DEFAULT NULL, route_params JSON DEFAULT NULL, INDEX IDX_6000B0D3E92F8F78 (recipient_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE notifications ADD CONSTRAINT FK_6000B0D3E92F8F78 FOREIGN KEY (recipient_id) REFERENCES user (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE favoris_evenement ADD CONSTRAINT FK_D95B32FD2C115A61 FOREIGN KEY (id_evenement_id) REFERENCES evenement (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE missions ADD CONSTRAINT FK_34F1D47E166D1F9C FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE missions ADD CONSTRAINT FK_34F1D47EB03A8386 FOREIGN KEY (created_by_id) REFERENCES user (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE missions ADD CONSTRAINT FK_34F1D47E89EEAF91 FOREIGN KEY (assigned_to) REFERENCES user (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE offre CHANGE date_creation date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE projects ADD CONSTRAINT FK_5C93B3A4B03A8386 FOREIGN KEY (created_by_id) REFERENCES user (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reclamation ADD CONSTRAINT FK_CE606404FB88E14F FOREIGN KEY (utilisateur_id) REFERENCES user (id) ON DELETE CASCADE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reponse ADD CONSTRAINT FK_5FB6DEC72D6BA2D9 FOREIGN KEY (reclamation_id) REFERENCES reclamation (id) ON DELETE CASCADE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation_evenement ADD CONSTRAINT FK_116109816B3CA4B FOREIGN KEY (id_user) REFERENCES user (id) ON DELETE CASCADE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation_evenement ADD CONSTRAINT FK_116109818B13D439 FOREIGN KEY (id_evenement) REFERENCES evenement (id_evenement) ON DELETE CASCADE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation_trajet ADD CONSTRAINT FK_63AAE0174A4A3511 FOREIGN KEY (vehicule_id) REFERENCES vehicule (id) ON DELETE CASCADE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation_trajet ADD CONSTRAINT FK_63AAE017D12A823 FOREIGN KEY (trajet_id) REFERENCES trajet (id) ON DELETE CASCADE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation_trajet ADD CONSTRAINT FK_63AAE017A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE vehicule ADD CONSTRAINT FK_292FFF1DD12A823 FOREIGN KEY (trajet_id) REFERENCES trajet (id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE notifications DROP FOREIGN KEY FK_6000B0D3E92F8F78
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE notifications
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE favoris_evenement DROP FOREIGN KEY FK_D95B32FD2C115A61
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE missions DROP FOREIGN KEY FK_34F1D47E166D1F9C
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE missions DROP FOREIGN KEY FK_34F1D47EB03A8386
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE missions DROP FOREIGN KEY FK_34F1D47E89EEAF91
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE offre CHANGE date_creation date_creation DATETIME DEFAULT CURRENT_TIMESTAMP
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE projects DROP FOREIGN KEY FK_5C93B3A4B03A8386
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reclamation DROP FOREIGN KEY FK_CE606404FB88E14F
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reponse DROP FOREIGN KEY FK_5FB6DEC72D6BA2D9
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation_evenement DROP FOREIGN KEY FK_116109816B3CA4B
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation_evenement DROP FOREIGN KEY FK_116109818B13D439
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation_trajet DROP FOREIGN KEY FK_63AAE0174A4A3511
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation_trajet DROP FOREIGN KEY FK_63AAE017D12A823
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation_trajet DROP FOREIGN KEY FK_63AAE017A76ED395
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE vehicule DROP FOREIGN KEY FK_292FFF1DD12A823
+        SQL);
+    }
+}
